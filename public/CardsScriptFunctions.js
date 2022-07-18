e10d15ad762cb35834f74c3087bdf94de38ae311
g7240 <107947650+g7240@users.noreply.github.com>
2022-07-18 21:20:49 +0200

    Ricarico tutto per sicurezza
    Sistemo il back to top button

--- public/CardsScriptFunctions.js
+++ public/CardsScriptFunctions.js
@@ -10,7 +10,7 @@ function swap_card(n,m){
 function CardsMenager(urls,title){
     let i
     if(localStorage.hasOwnProperty('url0')==false){
-        localStorage.setItem("name1","NEW YOTK CITY")
+        localStorage.setItem("name1","NEW YORK CITY")
         localStorage.setItem("url1","https://media.istockphoto.com/photos/skyline-of-downtown-new-york-brooklin-bridge-and-manhattan-at-the-picture-id1225580270?b=1&k=20&m=1225580270&s=170667a&w=0&h=rgG_3icBA-kM24hDMFKyHoMr0c8u2alqqVj3pv6q684=")
         localStorage.setItem("goto1","search?city=New+York+City&country=US")
 
@@ -18,7 +18,7 @@ function CardsMenager(urls,title){
         localStorage.setItem("url2","https://media.istockphoto.com/photos/aerial-view-of-london-and-the-tower-bridge-picture-id1265900812?b=1&k=20&m=1265900812&s=170667a&w=0&h=jOR9G-887Sh5LXygPFBIy8TIx5vCoVDm8FKISpapq-E=")//https://www.viaggilondra.it/wp-content/uploads/2016/04/meteo-londra.jpg
         localStorage.setItem("goto2","search?city=London&country=UK")
 
-        localStorage.setItem("name3","TOKIO")
+        localStorage.setItem("name3","TOKYO")
         localStorage.setItem("url3","https://media.istockphoto.com/photos/kabukicho-shinjuku-at-night-picture-id1330558677?b=1&k=20&m=1330558677&s=170667a&w=0&h=10l4A6mZSQ2-LvaRTwzCjIi7D057rY7HDmwgkOiLoZo=")
         localStorage.setItem("goto3","search?city=Tokyo&country=JP")
     }
